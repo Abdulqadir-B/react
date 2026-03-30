6f59ad8d91830bf160c2fce15526c5d411d4450c
Abdulqadir <aqbearing@gmail.com>
2026-03-30 20:06:03 +0530

    setup Tailwind CSS and implemented basic React props

---
+++ tailwind-props/src/App.jsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+import './App.css'
+import Card from './components/card'
+
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2 className="mt-40 underline">Getting started with using the Tailwind CSS under react</h2>
+      <Card title="sunset" />
+    </>
+  )
+}
+
+export default App
